8e18e5f2ad8ce03dc1c26412db306aa605a15810
Luan Caius <luancaius@outlook.com>
2019-03-27 19:31:27 -0400

    refactoring bandit v1

--- bandit_v1.py
+++ bandit_v1.py
@@ -1,84 +1,80 @@
 import numpy as np
 import matplotlib.pyplot as plt
 import random
-
-
-class MapAgent:
-    def __init__(self, total):
-        self.total = total
-        self.map = [0]*total
-
-    def getMaxIndex(self):
-        i = np.argmax([b for b in self.map])
-        return i
-
-    def random(self):
-        i = np.random.choice(self.total)
-        return i
+from random import randint
 
 
 class Agent:
-    def __init__(self):
-        self.reward = 0
+    def __init__(self, total, interactions, greedyness):
         self.env = {}
-        self.actions = {}
-        self.greedyness = 0.5
-        self.map = {}
+        self.total = total
+        self.interactions = interactions
+        self.greedyness = greedyness
+        self.arrayReward = [
+            [0 for i in range(total)] for j in range(interactions)]
+        self.arrayMean = [[0 for i in range(total)]
+                          for j in range(interactions)]
+        self.currentMean = [0 for i in range(total)]
+        self.index = 0
 
-    def run(self, totalInteractions):
-        totalBandits = self.env.totalBandits
-        self.map = MapAgent(totalBandits)
-
-        for i in range(totalInteractions):
-            reward = self.act(i)
-            self.reward += reward
-        return self.reward
+    def calculateMean(self, j):
+        sumArray = 0
+        totalIndex = 0
+        for i in range(self.index+1):
+            item = self.arrayReward[i][j]
+            sumArray += item
+            if item != 0:
+                totalIndex += 1
+        mean = sumArray/totalIndex
+        self.arrayMean[self.index][j] = mean
+        self.currentMean[j] = mean
 
     def act(self, i):
-        reward = 0
+        self.index = i
         p = random.uniform(0, 1)
-        if p < self.greedyness:
-            j = self.map.getMaxIndex()
+        if p > self.greedyness or i == 0:
+            j = randint(0, self.total-1)
         else:
-            j = self.map.random()
+            j = np.argmax(self.currentMean)
         reward = self.env.pull(j)
-        print((p < self.greedyness), " - ", j, " r=", reward)
+        self.arrayReward[i][j] = reward
+        self.calculateMean(j)
+        # print(('explore' if p > self.greedyness else 'greedy'),
+        #      ' ', i, ' ', j, ' ', self.currentMean)
         return reward
 
     def show(self):
-        print('--------Agent--------')
-        print(self.reward)
+        for i in range(self.interactions):
+            print("reward", self.arrayReward[i])
+        for i in range(self.interactions):
+            print("mean", self.arrayMean[i])
 
 
 class Bandit:
     def __init__(self, prob, i):
         self.id = i
-        self.sum = 0
         self.prob = prob
-        self.N = 0
-        self.mean = 0
 
     def pull(self):
-        self.N += 1
         randomNumber = random.uniform(0, 1)
-        reward = 0
-        if self.prob > randomNumber:
-            reward = 10
-        self.sum += reward
-        self.mean = self.sum/self.N
+        reward = self.prob*randomNumber*10
         return reward
-
-    def show(self):
-        print(self.id, ' mean = ', self.mean, ' N = ', self.N)
 
 
 class Env:
+    def __init__(self, x, total):
+        self.arrayReward = [[0 for i in range(x)] for j in range(total)]
+        self.arrayMean = [[0 for i in range(x)] for j in range(total)]
+        self.maxReward = [0 for i in range(x)]
+        self.minReward = [0 for i in range(x)]
+        self.index = 0
+        self.x = x
+        self.total = total
+
     def set_bandit(self, totalBandits):
         self.bandits = []
-        self.totalBandits = totalBandits
         self.create(totalBandits)
-        self.currentBandit = self.bandits[0]
-        self.index = 0
+        self.totalBandits = totalBandits
 
     def create(self, total):
         for i in range(total):
@@ -86,31 +82,48 @@ class Env:
             bandit = Bandit(prob, i)
             self.bandits.append(bandit)
 
-    def show(self):
-        print('--------Env--------')
-        for i in range(self.totalBandits):
-            prob = self.bandits[i].prob
-            print(i, ' ', prob)
-
     def pull(self, i):
-        self.currentBandit = self.bandits[i]
-        reward = self.currentBandit.pull()
-        self.currentBandit.show()
+        for n in range(self.totalBandits):
+            self.arrayReward[self.index][n] = self.bandits[n].pull()
+        reward = self.arrayReward[self.index][i]
+        self.index += 1
         return reward
 
-    def totalBandits(self):
-        return self.totalBandits
+    def calculateMean(self):
+        for j in range(self.x):
+            sumArray = 0
+            maxReward = 0
+            minReward = 1000000
+            for i in range(self.total):
+                item = self.arrayReward[i][j]
+                sumArray += item
+                self.arrayMean[i][j] = sumArray/(i+1)
+                if minReward > item:
+                    minReward = item
+                if maxReward < item:
+                    maxReward = item
+
+            self.minReward[j] = minReward
+            self.maxReward[j] = maxReward
+
+    def show(self):
+        for i in range(self.total):
+            print("env reward", self.arrayReward[i])
+        for i in range(self.total):
+            print("env mean", self.arrayMean[i])
+        print("min ", self.minReward)
+        print("max ", self.maxReward)
 
 
 if __name__ == "__main__":
-    env = Env()
-    env.set_bandit(10)
-    env.show()
-    total = 0
-    for i in range(10):
-        agent = Agent()
-        agent.env = env
-        reward = agent.run(1000)
-        print(reward)
-        total += reward
-    print(total)
+    total = 1000
+    bandit = 10
+    env = Env(bandit, total)
+    env.set_bandit(bandit)
+    agent = Agent(bandit, total, 0.1)
+    agent.env = env
+    for i in range(total):
+        agent.act(i)
+    agent.show()
+    agent.env.calculateMean()
+    agent.env.show()
